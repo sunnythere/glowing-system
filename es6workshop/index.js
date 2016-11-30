2344c55471ca75736afe9cc9e3ea137bf73ed8a0
Ya-wen Alice <yawenalice@gmail.com>
2016-11-30 11:34:44 -0500

    started index

---
+++ es6workshop/index.js
@@ -0,0 +1,13 @@
+class Character {
+	constructor (attackPower, health) {
+		this.attackPower = attackPower
+		this.health = health
+	}
+	attack (opposingPlayer) {
+		if (Math.random() > 0.5) {
+			//attack
+		}
+	}
+
+}
+
