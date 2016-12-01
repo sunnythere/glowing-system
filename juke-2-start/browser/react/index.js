f356c3578a42d987800ab7fb44d9acb6626cc22a
Ya-wen Alice <yawenalice@gmail.com>
2016-12-01 18:33:36 -0500

    props children

--- juke-2-start/browser/react/index.js
+++ juke-2-start/browser/react/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import AppContainer from './containers/AppContainer';
 
 ReactDOM.render(
-  <AppContainer />,
+  <Router history={hashHistory}>
+    <Route path="/" component={AppContainer}>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
